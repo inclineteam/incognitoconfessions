51bbfcd16e40b8be9aab680db4c4faccd3cafa46
ishmael cascabel <cascabel_ishmael@yahoo.com>
2022-07-24 12:57:13 +0800

    ADD AND UPDATE FILES
    rename landing page blade template and changes on route

---
+++ resources/views/incognitoLanding.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Incognito</title>
+        @vite('resources/css/app.css')
+
+    </head>
+    <body class="bg-[#1B1C21]">
+        <div>
+            <p class="text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, eveniet ducimus! Quas, excepturi quod officiis exercitationem, nostrum nulla nemo inventore eos quia suscipit delectus rem. Eligendi pariatur quas ipsum minima soluta hic a? Officiis sapiente totam iusto magnam ducimus a laborum quibusdam vitae porro velit, cupiditate commodi dolores molestiae voluptatem dolorum quidem aut sed tenetur numquam sunt aliquam ut repudiandae dolore! Suscipit repudiandae porro impedit similique at quae nulla molestias error dignissimos voluptatem, unde, laborum aspernatur? Sint quod, nisi odit, voluptate dolore sed, doloremque architecto assumenda molestias veritatis rerum sit? Ab ducimus eaque minus. Dolorum quod perspiciatis fugiat quos distinctio!</p>
+        </div>
+    </body>
+</html>
