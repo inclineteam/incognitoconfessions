5abb924c640d9a69df0f719cfb33bd48a7c82c35
ishmael cascabel <cascabel_ishmael@yahoo.com>
2022-07-24 12:43:27 +0800

    Add Fresh Install To development

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./resources/**/*.blade.php",
+  "./resources/**/*.js",
+  "./resources/**/*.vue",],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+
+
