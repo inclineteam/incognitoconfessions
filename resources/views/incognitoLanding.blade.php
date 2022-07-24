1164bec9fac986cc1ad0fc5107801d2750031ce0
ishmael cascabel <cascabel_ishmael@yahoo.com>
2022-07-24 13:48:59 +0800

    Update incognitoLanding.blade.php

--- resources/views/incognitoLanding.blade.php
+++ resources/views/incognitoLanding.blade.php
@@ -9,7 +9,7 @@
     </head>
     <body class="bg-[#1B1C21]">
         <div>
-            <p class="text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, eveniet ducimus! Quas, excepturi quod officiis exercitationem, nostrum nulla nemo inventore eos quia suscipit delectus rem. Eligendi pariatur quas ipsum minima soluta hic a? Officiis sapiente totam iusto magnam ducimus a laborum quibusdam vitae porro velit, cupiditate commodi dolores molestiae voluptatem dolorum quidem aut sed tenetur numquam sunt aliquam ut repudiandae dolore! Suscipit repudiandae porro impedit similique at quae nulla molestias error dignissimos voluptatem, unde, laborum aspernatur? Sint quod, nisi odit, voluptate dolore sed, doloremque architecto assumenda molestias veritatis rerum sit? Ab ducimus eaque minus. Dolorum quod perspiciatis fugiat quos distinctio!</p>
+            <!-- Content -->
         </div>
     </body>
 </html>
