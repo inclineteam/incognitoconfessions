838ccee509a537b876780d617033e1f59d178904
John Aeron Sencil <senciljohnaeron@gmail.com>
2022-07-25 15:58:52 +0800

    major changes
    - Modified header
    - incognitoLanding -> landing
    - Make use of blade components
    - Added confession wall

---
+++ resources/views/components/landing/hero.blade.php
@@ -0,0 +1,27 @@
+<div class="mx-auto flex max-w-6xl flex-col items-center pt-16 pb-56">
+    <div>
+        <div
+            class="mb-4 flex max-w-[25ch] items-center justify-center space-x-4 text-center text-7xl font-semibold tracking-tight text-white/80">
+            <span>Confess</span>
+            <span class="block rounded-lg bg-[#292A2F] px-4 py-3 font-medium text-white">without</span>
+            <span>them</span>
+        </div>
+        <div
+            class="flex max-w-[25ch] items-center space-x-4 text-center text-7xl font-semibold tracking-tight text-white/80">
+            <div class="relative">
+                <div class="rounded-lg bg-[#292A2F] px-4 py-4 font-medium text-white">having to know</div>
+                <div class="absolute top-full left-1/2 mt-3 flex -translate-x-1/2 items-center justify-center space-x-4">
+                    <i class="ai-check text-sm"></i>
+                    <p class='text-base font-normal tracking-normal text-white/50'>Confessions are anonymous</p>
+                </div>
+            </div>
+            <span>who you are.</span>
+        </div>
+    </div>
+    <p class="my-24 text-xl text-white/80">Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat.</p>
+
+    <div class="flex items-center space-x-10">
+        <button class="text-white">How does it work?</button>
+        <button class="rounded-md bg-[#3B50F9] px-6 py-4 font-medium text-white">Write a confession</button>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('incognitoLanding');
+    return view('pages.landing');
 });
