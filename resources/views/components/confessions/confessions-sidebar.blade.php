ef8b9758169b5f6ebe8b42c490dec66dbd77e16f
ishmael cascabel <cascabel_ishmael@yahoo.com>
2022-07-28 15:37:19 +0800

    Update View All Confessions Page

---
+++ resources/views/components/confessions/confessions-sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="flex flex-col bg-[#161515] w-[5rem] h-[100vh] sticky justify-between top-0 items-center">
+    <div class="flex h-[8rem] w-[100%] items-center justify-center">
+        <a href="/"><img src="images/ai-glass.png" class="h-[50px] w-[50px]"></a>
+    </div>
+    <div class="flex h-[75%]">
+        
+    </div>
+    <div class="flex h-[5rem] w-[100%] items-center justify-center">
+        <img src="images/logout.png" class="h-[20px] w-[20px]">
+    </div>
+</div>
--- resources/views/components/confessions/view-confessions.blade.php
+++ resources/views/components/confessions/view-confessions.blade.php
@@ -1,23 +1,18 @@
 @props(["confessions" => $confessions])
 
-<div>
-    <!-- TODO -->
+<div class="flex flex-col  items-center">
+    <div class="relative mx-auto max-w-6xl columns-3 gap-6">
 
-    <div class="flex flex-col h-[250vh]">
-        <div class="relative mx-auto max-w-6xl columns-3 gap-6">
-
-            @if (count($confessions) > 0)
-            
-                @foreach ($confessions as $confession)
-                    <x-confessions.view-confession-card :confession="$confession" :date="Carbon\Carbon::parse($confession->created_at)->format('m/d/y')"/>
-                @endforeach
-                
-            @endif
+        @if (count($confessions) > 0)
         
-        </div>
-        <div class="text-white justify-center flex w-[100%]">
-            {{ $confessions->links('vendor.pagination.custom') }}
-        </div>
+            @foreach ($confessions as $confession)
+                <x-confessions.view-confession-card :confession="$confession" :date="Carbon\Carbon::parse($confession->created_at)->format('m/d/y')"/>
+            @endforeach
+            
+        @endif
+    
     </div>
-
+    <div class="text-white justify-center flex w-[100%]">
+        {{ $confessions->links('vendor.pagination.custom') }}
+    </div>
 </div>
