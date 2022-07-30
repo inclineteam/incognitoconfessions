7e112d38b5db89e28c8b719c6adf06b996af16ad
ishmael cascabel <cascabel_ishmael@yahoo.com>
2022-07-30 13:11:26 +0800

    update project
    delete files

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('pages.landing');
-})->middleware('guest');
+Route::get('/', [landingconfessionsController::class, "index"])->middleware("guest");
 
 Route::get('/home', function () {
     return view('pages.home');
@@ -24,5 +22,4 @@ Route::get('/home', function () {
 
 require __DIR__ . '/auth.php';
 
-Route::get('/', [landingconfessionsController::class, "index"]);
 Route::get('/confessions', [landingconfessionsController::class, "confessions"]);
