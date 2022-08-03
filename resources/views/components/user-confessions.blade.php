2d4549a16bee3c4360d9408d9fe1c6d4a4991d87
ishmael cascabel <cascabel_ishmael@yahoo.com>
2022-08-03 13:07:16 +0800

    Homepage CRUD complete
    homepage crud complete
       - find bugs
       - add captcha on create confessions to avoid spam

--- resources/views/components/user-confessions.blade.php
+++ resources/views/components/user-confessions.blade.php
@@ -1,6 +1,12 @@
 @props(["confessions" => $confessions])
-<div>
-    @foreach ($confessions as $confession)
-        <x-confession-card :confession="$confession" />
-    @endforeach
+<div class="h-[80%]">
+    @if (count($confessions) > 0)
+        @foreach ($confessions as $confession)
+            <x-confession-card :confession="$confession" />
+        @endforeach
+    @else
+        <div class="flex h-[100%] items-center justify-center">
+            <p class="text-center text-2xl text-white/50">No confessions</p>
+        </div>
+    @endif
 </div>
