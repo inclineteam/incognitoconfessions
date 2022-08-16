9f51f5e33b1d20b80271688d785a95cd72c3cc03
John Aeron Sencil <senciljohnaeron@gmail.com>
2022-08-16 15:05:54 +0800

    Responsive landing page
    - only show "Confessions are anonymous" in lg or bigger breakpoint
    - added mobile nav (dropdown) for xs or smaller breakpoints

--- resources/views/components/landing/confession-card.blade.php
+++ resources/views/components/landing/confession-card.blade.php
@@ -3,14 +3,16 @@
 <div class="relative mb-6 inline-block w-full overflow-hidden rounded-lg bg-zinc-800/90 p-6">
     <img src="/images/quote.svg" alt="" class="absolute -top-10 left-0 h-36 w-36 select-none" />
 
-    <div class="flex h-[4rem] items-center justify-between text-sm">
-        <p class="font-medium text-white/50">{{ $confession->name }}</p>
+    <div class="flex items-center justify-between pt-6 pb-2 lg:pb-6 text-sm">
+        <p class="font-medium text-zinc-400">{{ $confession->name }}</p>
     </div>
-    <p class="relative mb-16 whitespace-pre-wrap text-lg italic text-white/80">{{ $confession->content }}</p>
+    <p class="relative mb-8 whitespace-pre-wrap text-base italic text-zinc-200 md:mb-12 lg:mb-16 lg:text-lg">
+{{ $confession->content }}
+    </p>
 
     <div class="flex items-center justify-between text-sm">
-        <p class="font-medium text-white/50">To {{ $confession->to }}</p>
+        <p class="font-medium text-zinc-400">To {{ $confession->to }}</p>
 
-        <span class="text-white/30">{{ Carbon\Carbon::parse($confession->created_at)->format('m/d/y') }}</span>
+        <span class="text-zinc-600">{{ Carbon\Carbon::parse($confession->created_at)->format('m/d/y') }}</span>
     </div>
 </div>
